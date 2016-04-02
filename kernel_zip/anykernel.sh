9e3d93f296deaad3051420bcae1423415e859b9d
Syukri Lajin <ginger_boy92@hotmail.com>
2016-04-02 10:51:26 +0800

    updates

--- kernel_zip/anykernel.sh
+++ kernel_zip/anykernel.sh
@@ -1,9 +1,9 @@
-kj# AnyKernel 2.0 Ramdisk Mod Script 
+# AnyKernel2 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
 ## AnyKernel setup
 # EDIFY properties
-kernel.string=God s Kernel by Tarun93 @ xda-developers
+kernel.string=gods kernel by tarun93
 do.devicecheck=1
 do.initd=1
 do.modules=1
@@ -28,32 +28,34 @@ split_img=/tmp/anykernel/split_img;
 patch=/tmp/anykernel/patch;
 bindir=/system/bin;
 
-chmod -R 755 $bin;
+chmod -R 755 $bin $ramdisk/sbin;
 mkdir -p $ramdisk $split_img;
-cd $ramdisk;
 
-OUTFD=`ps | grep -v "grep" | grep -oE "update(.*)" | cut -d" " -f3`;
-ui_print() { echo "ui_print $1" >&$OUTFD; echo "ui_print" >&$OUTFD; }
+OUTFD=/proc/self/fd/$1;
+ui_print() { echo -e "ui_print $1\nui_print" > $OUTFD; }
 
 # dump boot and extract ramdisk
 dump_boot() {
   dd if=$block of=/tmp/anykernel/boot.img;
   $bin/unpackbootimg -i /tmp/anykernel/boot.img -o $split_img;
   if [ $? != 0 ]; then
-    ui_print " "; ui_print "Dumping/unpacking image failed. Aborting...";
-    echo 1 > /tmp/anykernel/exitcode; exit;
+    ui_print " "; ui_print "Dumping/splitting image failed. Aborting..."; exit 1;
   fi;
+  mv -f $ramdisk /tmp/anykernel/rdtmp;
+  mkdir -p $ramdisk;
+  cd $ramdisk;
   gunzip -c $split_img/boot.img-ramdisk.gz | cpio -i;
+  if [ $? != 0 -o -z "$(ls $ramdisk)" ]; then
+    ui_print " "; ui_print "Unpacking ramdisk failed. Aborting..."; exit 1;
+  fi;
+  cp -af /tmp/anykernel/rdtmp/* $ramdisk;
 }
 
 # repack ramdisk then build and write image
 write_boot() {
   cd $split_img;
   cmdline=`cat *-cmdline`;
-  #found=$(find *-cmdline -type f | xargs grep -oh "androidboot.selinux=permissive");
-  #if [ "$found" != 'androidboot.selinux=permissive' ]; then
-  #cmdline="$cmdline androidboot.selinux=permissive";
-  #fi
+  cmdline="$cmdline androidboot.selinux=permissive";
   board=`cat *-board`;
   base=`cat *-base`;
   pagesize=`cat *-pagesize`;
@@ -80,10 +82,14 @@ write_boot() {
   fi;
   cd $ramdisk;
   find . | cpio -H newc -o | gzip > /tmp/anykernel/ramdisk-new.cpio.gz;
+  if [ $? != 0 ]; then
+    ui_print " "; ui_print "Repacking ramdisk failed. Aborting..."; exit 1;
+  fi;
   $bin/mkbootimg --kernel $kernel --ramdisk /tmp/anykernel/ramdisk-new.cpio.gz $second --cmdline "$cmdline" --board "$board" --base $base --pagesize $pagesize --kernel_offset $kerneloff --ramdisk_offset $ramdiskoff $secondoff --tags_offset $tagsoff $dtb --output /tmp/anykernel/boot-new.img;
-  if [ $? != 0 -o `wc -c < /tmp/anykernel/boot-new.img` -gt `wc -c < /tmp/anykernel/boot.img` ]; then
-    ui_print " "; ui_print "Repacking image failed. Aborting...";
-    echo 1 > /tmp/anykernel/exitcode; exit;
+  if [ $? != 0 ]; then
+    ui_print " "; ui_print "Repacking image failed. Aborting..."; exit 1;
+  elif [ `wc -c < /tmp/anykernel/boot-new.img` -gt `wc -c < /tmp/anykernel/boot.img` ]; then
+    ui_print " "; ui_print "New image larger than boot partition. Aborting..."; exit 1;
   fi;
   dd if=/tmp/anykernel/boot-new.img of=$block;
 }
@@ -98,7 +104,7 @@ replace_string() {
   fi;
 }
 
-# insert_line <file> <if search string> <before/after> <line match string> <inserted line>
+# insert_line <file> <if search string> <before|after> <line match string> <inserted line>
 insert_line() {
   if [ -z "$(grep "$2" $1)" ]; then
     case $3 in
@@ -106,7 +112,7 @@ insert_line() {
       after) offset=1;;
     esac;
     line=$((`grep -n "$4" $1 | cut -d: -f1` + offset));
-    sed -i "${line}s;^;${5};" $1;
+    sed -i "${line}s;^;${5}\n;" $1;
   fi;
 }
 
@@ -133,6 +139,19 @@ prepend_file() {
   fi;
 }
 
+# insert_file <file> <if search string> <before|after> <line match string> <patch file>
+insert_file() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    case $3 in
+      before) offset=0;;
+      after) offset=1;;
+    esac;
+    line=$((`grep -n "$4" $1 | cut -d: -f1` + offset));
+    sed -i "${line}s;^;\n;" $1;
+    sed -i "$((line - 1))r $patch/$5" $1;
+  fi;
+}
+
 # append_file <file> <if search string> <patch file>
 append_file() {
   if [ -z "$(grep "$2" $1)" ]; then
@@ -144,8 +163,24 @@ append_file() {
 
 # replace_file <file> <permissions> <patch file>
 replace_file() {
-  cp -fp $patch/$3 $1;
+  cp -pf $patch/$3 $1;
   chmod $2 $1;
+}
+
+# patch_fstab <fstab file> <mount match name> <fs match type> <block|mount|fstype|options|flags> <if search string> <replacement string>
+patch_fstab() {
+  entry=$(grep "$2" $1 | grep "$3");
+  if [ -z "$(echo "$entry" | grep "$5")" ]; then
+    case $4 in
+      block) part=$(echo "$entry" | awk '{ print $1 }');;
+      mount) part=$(echo "$entry" | awk '{ print $2 }');;
+      fstype) part=$(echo "$entry" | awk '{ print $3 }');;
+      options) part=$(echo "$entry" | awk '{ print $4 }');;
+      flags) part=$(echo "$entry" | awk '{ print $5 }');;
+    esac;
+    newentry=${entry//$part/$6};
+    sed -i "s;${entry};${newentry};" $1;
+  fi;
 }
 
 ## end methods
@@ -154,41 +189,44 @@ replace_file() {
 ## AnyKernel permissions
 # set permissions for included files
 chmod -R 755 $ramdisk
+#chmod 644 $ramdisk/sbin/media_profiles.xml
 
 ## Remove stock MPD and Thermal Binaries
+#
 mv $bindir/mpdecision $bindir/mpdecision-bak
-#mv $bindir/thermal-engine $bindir/thermal-engine-bak
+mv $bindir/thermal-engine $bindir/thermal-engine-bak
 rm -rf $bindir/../lib/modules/*
-
-# restore mpdecision! // use this if you decides to go back to mpdecision. don't forget to disable the one above first though....
-#mv $bindir/mpdecision-bak $bindir/mpdecision 
 
 ## AnyKernel install
 dump_boot;
 
 # begin ramdisk changes
-cp -fp $patch/* /system/etc/
-chmod 755 /system/etc/init.qcom.post_boot.sh
 
-#replace fstab
-#ui_print() " ";ui_print() "Replacing fstab..........";
-#  backup_file fstab.qcom;
-#  replace_file fstab.qcom 644 fstab.qcom;
+# init.superuser.rc
+#if [ -f init.superuser.rc ]; then
+#  backup_file init.superuser.rc;
+#  replace_string init.superuser.rc "Superuser su_daemon" "# su daemon" "\n# Superuser su_daemon";
+#  prepend_file init.superuser.rc "SuperSU daemonsu" init.superuser;
+#else
+#  replace_file init.superuser.rc 750 init.superuser.rc;
+#  insert_line init.rc "init.superuser.rc" after "on post-fs-data" "    import /init.superuser.rc";
+#fi;
 
 # adb secure
 backup_file default.prop;
 replace_string default.prop "ro.adb.secure=0" "ro.adb.secure=1" "ro.adb.secure=0";
 replace_string default.prop "ro.secure=0" "ro.secure=1" "ro.secure=0";
 
-# Add custom loader script
-found=$(find init.rc -type f | xargs grep -oh "import /init.custom.rc");
-if [ "$found" != 'import /init.custom.rc' ]; then
-	#append the new lines for this option at the bottom
-        echo "" >> init.rc
-	echo "import /init.custom.rc" >> init.rc
-fi
+#sswap
+chmod 750 $ramdisk/sbin/sswap
 
-# end ramdisk changes
+# Add custom loader script
+found=$(find init.rc -type f | xargs grep -oh "import /init.god.rc");
+if [ "$found" != 'import /init.god.rc' ]; then
+#append the new lines for this option at the bottom
+	echo "import /init.god.rc" >> init.rc
+fi
+ end ramdisk changes
 
 write_boot;
 
--- kernel_zip/ramdisk/post_boot.sh
+++ kernel_zip/ramdisk/post_boot.sh
@@ -16,15 +16,6 @@ mount -o remount,rw -t auto /
 mount -o remount,rw -t auto /system
 mount -t rootfs -o remount,rw rootfs
 
-if [ -f $BBX ]; then
-	chown 0:2000 $BBX
-	chmod 0755 $BBX
-	$BBX --install -s /system/xbin
-	ln -s $BBX /sbin/busybox
-	ln -s $BBX /system/bin/busybox
-	sync
-fi
-
 # Set environment and create symlinks: /bin, /etc, /lib, and /etc/mtab
 set_environment ()
 {
@@ -54,46 +45,9 @@ if [ -x $BBX ]; then
 fi
 
 ########################################################
-# kernel custom test
+#Supersu
 #
-
-if [ -e /data/godtest.log ]; then
-	rm /data/godtest.log
-fi
-
-echo  Kernel script is working !!! >> /data/godtest.log
-echo "excecuted on $(date +"%d-%m-%Y %r" )" >> /data/godtest.log
-echo  Done ! >> /data/godtest.log
-
-########################################################
-#FSTRIM
-#
-$BBX fstrim -v /system >> /data/godtest.log
-$BBX fstrim -v /cache >> /data/godtest.log
-$BBX fstrim -v /data >> /data/godtest.log
-
-
-########################################################
-# Scheduler and Read Ahead
-#
-echo bfq > /sys/block/mmcblk0/queue/scheduler
-echo 512 > /sys/block/mmcblk0/bdi/read_ahead_kb
-	# LOCK IT
-	chmod 644 /sys/block/mmcblk0/queue/scheduler
-
-########################################################
-# LMK Tweaks
-#
-echo "2560,4096,8192,16384,24576,32768" > /sys/module/lowmemorykiller/parameters/minfree
-echo "32" > /sys/module/lowmemorykiller/parameters/cost
-
-########################################################
-# Tweak Background Writeout
-#
-echo "200" > /proc/sys/vm/dirty_expire_centisecs
-echo "40" > /proc/sys/vm/dirty_ratio
-echo "5" > /proc/sys/vm/dirty_background_ratio
-echo "0" > /proc/sys/vm/swappiness
+/system/xbin/daemonsu --auto-daemon &
 
 ########################################################
 # initialize init.d
@@ -105,49 +59,138 @@ fi;
 ########################################################
 # Allow untrusted apps to read from debugfs
 #
-#if [ -e /system/lib/libsupol.so ]; then
-#/system/xbin/supolicy --live \
-#	"allow untrusted_app debugfs file { open read getattr }" \
-#	"allow untrusted_app sysfs_lowmemorykiller file { open read getattr }" \
-#	"allow untrusted_app sysfs_devices_system_iosched file { open read getattr }" \	
-#	"allow untrusted_app persist_file dir { open read getattr }" \
-#	"allow debuggerd gpu_device chr_file { open read getattr }" \
-#	"allow netd netd capability fsetid" \
-#	"allow netd { hostapd dnsmasq } process fork" \
-#	"allow { system_app shell } dalvikcache_data_file file write" \
-#	"allow { zygote mediaserver bootanim appdomain }  theme_data_file dir { search r_file_perms r_dir_perms }" \
-#v	"allow { zygote mediaserver bootanim appdomain }  theme_data_file file { r_file_perms r_dir_perms }" \
-#	"allow system_server { rootfs resourcecache_data_file } dir { open read write getattr add_name setattr create remove_name rmdir unlink link }" \
-#	"allow system_server resourcecache_data_file file { open read write getattr add_name setattr create remove_name unlink link }" \
-#	"allow system_server dex2oat_exec file rx_file_perms" \
-#	"allow mediaserver mediaserver_tmpfs file execute" \
-#	"allow drmserver theme_data_file file r_file_perms" \
-#	"allow zygote system_file file write" \
-#	"allow atfwd property_socket sock_file write" \
-#	"allow untrusted_app sysfs_display file { open read write getattr add_name setattr remove_name }" \	
-#	"allow debuggerd app_data_file dir search" \
-#	"allow sensors diag_device chr_file { read write open ioctl }" \
-#	"allow sensors sensors capability net_raw" \
-#	"allow init kernel security setenforce" \
-#	"allow netmgrd netmgrd netlink_xfrm_socket nlmsg_write" \
-#	"allow netmgrd netmgrd socket { read write open ioctl }"
-#fi;
+if [ -e /system/lib/libsupol.so ]; then
+/system/xbin/supolicy --live \
+	"allow untrusted_app debugfs file { open read getattr }" \
+	"allow untrusted_app sysfs_lowmemorykiller file { open read getattr }" \
+	"allow untrusted_app sysfs_devices_system_iosched file { open read getattr }" \	
+	"allow untrusted_app persist_file dir { open read getattr }" \
+	"allow debuggerd gpu_device chr_file { open read getattr }" \
+	"allow netd netd capability fsetid" \
+	"allow netd { hostapd dnsmasq } process fork" \
+	"allow { system_app shell } dalvikcache_data_file file write" \
+	"allow { zygote mediaserver bootanim appdomain }  theme_data_file dir { search r_file_perms r_dir_perms }" \
+	"allow { zygote mediaserver bootanim appdomain }  theme_data_file file { r_file_perms r_dir_perms }" \
+	"allow system_server { rootfs resourcecache_data_file } dir { open read write getattr add_name setattr create remove_name rmdir unlink link }" \
+	"allow system_server resourcecache_data_file file { open read write getattr add_name setattr create remove_name unlink link }" \
+	"allow system_server dex2oat_exec file rx_file_perms" \
+	"allow mediaserver mediaserver_tmpfs file execute" \
+	"allow drmserver theme_data_file file r_file_perms" \
+	"allow zygote system_file file write" \
+	"allow atfwd property_socket sock_file write" \
+	"allow untrusted_app sysfs_display file { open read write getattr add_name setattr remove_name }" \	
+	"allow debuggerd app_data_file dir search" \
+	"allow sensors diag_device chr_file { read write open ioctl }" \
+	"allow sensors sensors capability net_raw" \
+	"allow init kernel security setenforce" \
+	"allow netmgrd netmgrd netlink_xfrm_socket nlmsg_write" \
+	"allow netmgrd netmgrd socket { read write open ioctl }"
+fi;
 
 ########################################################
 # Google Services battery drain fixer
 #
-pm enable com.google.android.gms/.update.SystemUpdateActivity
-pm enable com.google.android.gms/.update.SystemUpdateService
-pm enable com.google.android.gms/.update.SystemUpdateService$ActiveReceiver
-pm enable com.google.android.gms/.update.SystemUpdateService$Receiver
-pm enable com.google.android.gms/.update.SystemUpdateService$SecretCodeReceiver
-pm enable com.google.android.gsf/.update.SystemUpdateActivity
-pm enable com.google.android.gsf/.update.SystemUpdatePanoActivity
-pm enable com.google.android.gsf/.update.SystemUpdateService
-pm enable com.google.android.gsf/.update.SystemUpdateService$Receiver
-pm enable com.google.android.gsf/.update.SystemUpdateService$SecretCodeReceiver
 
-########################################################
-# Power Effecient Workqueues (Enable for battery)
+# stop google service and restart it on boot. this remove high cpu load and ram leak!
+
+	if [ "$($BBX pidof com.google.android.gms | wc -l)" -eq "1" ]; then
+		$BBX kill "$($BBX pidof com.google.android.gms)";
+	fi;
+	if [ "$($BBX pidof com.google.android.gms.unstable | wc -l)" -eq "1" ]; then
+		$BBX kill "$($BBX pidof com.google.android.gms.unstable)";
+	fi;
+	if [ "$($BBX pidof com.google.android.gms.persistent | wc -l)" -eq "1" ]; then
+		$BBX kill "$($BBX pidof com.google.android.gms.persistent)";
+	fi;
+	if [ "$($BBX pidof com.google.android.gms.wearable | wc -l)" -eq "1" ]; then
+		$BBX kill "$($BBX pidof com.google.android.gms.wearable)";
+	fi;
+	if [ "$($BBX pidof com.google.process.gapps | wc -l)" -eq "1" ]; then
+		$BBX kill "$($BBX pidof com.google.process.gapps)";
+	fi;
+	if [ "$($BBX pidof com.google.android.gsf | wc -l)" -eq "1" ]; then
+		$BBX kill "$($BBX pidof com.google.android.gms.wearable)";
+	fi;
+	if [ "$($BBX pidof com.google.android.gms.wearable | wc -l)" -eq "1" ]; then
+		$BBX kill "$($BBX pidof com.google.android.gsf.persistent)";
+	fi;
+
+$busybox sleep 10
+ pm enable com.google.android.gms/.update.SystemUpdateActivity
+ pm enable com.google.android.gms/.update.SystemUpdateService
+ pm enable com.google.android.gms/.update.SystemUpdateService$ActiveReceiver
+ pm enable com.google.android.gms/.update.SystemUpdateService$Receiver
+ pm enable com.google.android.gms/.update.SystemUpdateService$SecretCodeReceiver
+ pm enable com.google.android.gsf/.update.SystemUpdateActivity
+ pm enable com.google.android.gsf/.update.SystemUpdatePanoActivity
+ pm enable com.google.android.gsf/.update.SystemUpdateService
+ pm enable com.google.android.gsf/.update.SystemUpdateService$Receiver
+ pm enable com.google.android.gsf/.update.SystemUpdateService$SecretCodeReceiver
+
+#zram_turn_on='$(getprop sys.zram.enable)'
+zram_turn_on=1
+zram_size=768
+if [ $zram_turn_on == 1 ];then
+##
+#insmod /system/lib/modules/lz4_compress.ko
+#insmod /system/lib/modules/lz4_decompress.ko
+#insmod /system/lib/modules/zram.ko num_devices=4
+echo 1 > /sys/block/zram0/reset
+echo 4 > /sys/block/zram0/max_comp_streams
+echo "lz4" > /sys/block/zram0/comp_algorithm
+echo "$(($zram_size*1024*1024))" > /sys/block/zram0/disksize
+#echo "1610612736" > /sys/block/zram0/disksize
+echo "$(($zram_size*1024*1024))" > /sys/block/zram0/mem_limit
+mkswap /dev/block/zram0
+swapon -p 1 /dev/block/zram0
 #
-echo "1" > /sys/module/workqueue/parameters/power_efficient
+echo 100 > /proc/sys/vm/swappiness
+echo 60  > /proc/sys/vm/vfs_cache_pressure
+echo 100  > /proc/sys/vm/overcommit_ratio
+#echo 30 > /sys/module/zswap/parameters/max_pool_percent
+echo 0 > /proc/sys/vm/page-cluster
+fi;
+
+echo 1 > /sys/module/cpu_boost/parameters/input_boost_enabled
+echo 1036800 > sys/module/cpu_boost/parameters/input_boost_freq
+echo 500 > /sys/module/cpu_boost/parameters/input_boost_ms
+
+overclock=1
+if [ $overclock == 0 ];then
+	minfrequency=300000
+	maxfrequency=2265600
+else
+	minfrequency=268800
+	maxfrequency=2803200
+fi;
+
+echo &maxfrequency > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo &maxfrequency > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+echo &maxfrequency > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo &maxfrequency > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+
+#echo "intellimm" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+#echo "intellimm" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+#echo "intellimm" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+#echo "intellimm" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+
+echo 2 > /sys/devices/system/cpu/sched_mc_power_savings
+
+mako_hotplug=1
+if [ $mako_hotplug == 1 ];then
+ echo  1 > /sys/class/misc/mako_hotplug_control/enabled
+ echo 30 > /sys/class/misc/mako_hotplug_control/high_load_counter
+ echo 30 > /sys/class/misc/mako_hotplug_control/max_load_counter
+ echo  5  > /sys/class/misc/mako_hotplug_control/min_time_cpu_online
+ echo  2  > /sys/class/misc/mako_hotplug_control/timer
+ echo 98 > /sys/class/misc/mako_hotplug_control/load_threshold
+ echo 1497600 > /sys/class/misc/mako_hotplug_control/cpufreq_unplug_limit
+fi;
+
+alucard_hotplug=0
+if [ $alucard_hotplug == 1 ];then
+echo 1 > /sys/kernel/alucard_hotplug/hotplug_enable
+echo 20 > /sys/kernel/alucard_hotplug/cpu_down_rate
+echo 10 > /sys/kernel/alucard_hotplug/cpu_up_rate
+fi;
+
